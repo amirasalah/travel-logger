f371119d91b7bfbe5cd7e1321ff227cbe63d5740
amirasalah <amera.salah.89@gmail.com>
2020-12-02 15:31:10 +0200

    middlewares

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # travel-logger
+
 Side project to log travel destinations on a map using Express, Mango and React.
+
+The user of the app uses it to log his current location with the following details:
+
+* Title
+* Description
+* Latitude
+* Longitude
+* Start Date
+* End Date
+* Images slider
--- server/src/index.js
+++ server/src/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const morgan = require('morgan')
 const helmet = require('helmet')
 const cors = require('cors')
+const { notFound, errorHandler } = require('./middlewares')
 
 const app = express()
 
@@ -20,23 +21,8 @@ app.get('/', (req, res) => {
         message: 'Hello from the other side',
     })
 })
-app.use((req, res, next) => {
-    const error = new Error(`Not Found - ${req.originalUrl}`)
-    res.status(404)
-    next(error)
-})
-// eslint-disable-next-line no-unused-vars
-app.use((error, req, res, next) => {
-    const statusCode = res.statusCode === 200 ? 500 : res.statusCode
-    res.status(statusCode)
-    res.json({
-        message: error.message,
-        stack:
-            process.env.NODE_ENV === 'production'
-                ? "OPPS, we're sorry but Some error happened 🙊 🙈 🙉"
-                : error.stack,
-    })
-})
+app.use(notFound)
+app.use(errorHandler)
 
 const port = process.env.PORT || 1337
 
