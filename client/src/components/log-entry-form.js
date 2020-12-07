4286333b7a16c99f1f0218625cd518a3319836dc
amirasalah <amera.salah.89@gmail.com>
2020-12-07 19:51:22 +0200

    add tailwind

--- client/src/components/log-entry-form.js
+++ client/src/components/log-entry-form.js
@@ -27,22 +27,29 @@ const LogEntryForm = () => {
     //     })
     // }
     return (
-        <form className='entry-form' onSubmit={handleSubmit(onSubmit)}>
-            {/* {error && <h3 className='error'>{error}</h3>} */}
-            <label htmlFor='password'>Password</label>
-            <input type='password' name='password' required ref={register} />
-            <label htmlFor='title'>Title</label>
-            <input name='title' required ref={register} />
-            <label htmlFor='description'>Description</label>
-            <textarea name='description' rows={3} ref={register}></textarea>
-            <label htmlFor='image'>Image</label>
-            <input name='image' ref={register} />
-            <label htmlFor='visitDate'>Visit Date</label>
-            <input name='visitDate' type='date' required ref={register} />
-            {/* <button disabled={loading}>
+        <main className='container'>
+            <form className='entry-form' onSubmit={handleSubmit(onSubmit)}>
+                {/* {error && <h3 className='error'>{error}</h3>} */}
+                <label htmlFor='password'>Password</label>
+                <input
+                    type='password'
+                    name='password'
+                    required
+                    ref={register}
+                />
+                <label htmlFor='title'>Title</label>
+                <input name='title' required ref={register} />
+                <label htmlFor='description'>Description</label>
+                <textarea name='description' rows={3} ref={register}></textarea>
+                <label htmlFor='image'>Image</label>
+                <input name='image' ref={register} />
+                <label htmlFor='visitDate'>Visit Date</label>
+                <input name='visitDate' type='date' required ref={register} />
+                {/* <button disabled={loading}>
                 {loading ? 'Loading...' : 'Create Entry'}
             </button> */}
-        </form>
+            </form>
+        </main>
     )
 }
 export default LogEntryForm
